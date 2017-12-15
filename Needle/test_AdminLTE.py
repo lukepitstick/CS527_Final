f59be99571d4955469fe55084b8fcb39636c485b
Luke Pitstick <pitstile@miamioh.edu>
2017-12-15 04:48:59 -0600

    chromdriven test

--- Needle/test_AdminLTE.py
+++ Needle/test_AdminLTE.py
@@ -1,11 +1,7 @@
 from needle.cases import NeedleTestCase
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-#from needle.driver import NeedlePhantomJS
 
-opts = Options()
-opts.binary = "C:\Program Files (x86)\Mozilla Firefox\firefox.exe"
-driver = webdriver.Firefox(firefox_options=opts)
+driver = webdriver.Chrome('./chromedriver')
 
 class AdminLTETest(NeedleTestCase):
     viewport_width = 1024
@@ -16,6 +12,6 @@ class AdminLTETest(NeedleTestCase):
     #     return NeedlePhantomJS()
 
     def test_fullscreenmasthead(self):
-        #self.driver.get('http://localhost:8080')
-        self.driver.get('http://localhost/AdminLTE-master/index.html')
+        self.driver.get('http://localhost:8080')
+        # self.driver.get('http://localhost/AdminLTE-master/index.html')
         self.assertScreenshot('*', 'fullscreen')
